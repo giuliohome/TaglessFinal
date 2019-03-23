4b5833631b77ad10f5847c8e19dc0e10ab64a97b
giuliohome <giuliohome@gmail.com>
2019-03-23 18:14:00 +0100

    example implemented

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TaglessFinal
 F# Encoding
+
+Tagless Final Encoding in F# 
+
+Translated from [@jproyo Haskell version](https://jproyo.github.io/posts/2019-03-17-tagless-final-haskell.html)
+
+Based on [F# version of Type Classes](http://www.fssnip.net/7Tr/title/F-version-of-Type-Classes-from-OO-Perspective) from OO Perspective
---
+++ TypeClasses/Program.fs
@@ -0,0 +1,46 @@
+﻿open System
+
+open System
+
+type UserName = string
+
+type DataResult = DataResult of string
+
+type Cache =
+    abstract member getFromCache : string -> DataResult option
+    abstract member storeCache : string -> unit
+
+type DataSource =
+    abstract member getFromSource : string -> DataResult
+
+let requestDate (cacheImpl:Cache) (dataSourceimpl:DataSource) (userName:UserName) = 
+    match cacheImpl.getFromCache userName with
+    | Some dataResult -> dataResult
+    | None -> dataSourceimpl.getFromSource userName
+
+let CacheNotInCache =
+    { new Cache with
+        member this.getFromCache _ = None
+        member this.storeCache _ = () }
+
+let DataSourceeNotInCache = 
+    { new DataSource with
+          member this.getFromSource user = 
+              "source: " + user |> DataResult}
+
+let CacheInCache =
+    { new Cache with
+        member this.getFromCache user = 
+           "cache: " + user |> DataResult |> Some
+        member this.storeCache _ = () }
+
+let DataSourceeInCache = 
+    { new DataSource with
+          member this.getFromSource _  = 
+              raise (NotImplementedException())}
+              
+[<EntryPoint>]
+let main argv =
+    printfn "%A" <| requestDate CacheNotInCache DataSourceeNotInCache "john" 
+    printfn "%A" <| requestDate CacheInCache DataSourceeInCache "john" 
+    0 // return an integer exit code
