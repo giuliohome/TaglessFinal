cd94651b6e0e3e99872b3dd31f9ffc3f25115e9f
giuliohome <giuliohome@gmail.com>
2019-04-03 00:48:13 +0200

    Tagless Final with Statically Resolved Type Parameters

--- TypeClasses/Program.fs
+++ TypeClasses/Program.fs
@@ -3,57 +3,54 @@ open FSharpPlus
 
 type UserName = string
 type DataResult<'t> = DataResult of 't with
-    static member Map ( x:DataResult<'t>  , f) =
-        match x with 
-        | DataResult t -> DataResult (f t)
+    static member Map ( DataResult t  , f) = DataResult (f t)
 
-type Cache<'t> =
-    abstract member getFromCache : 't -> DataResult<'t> option
-    abstract member storeCache : 't -> unit
 
-[<AbstractClass>] // in case you want default implementation
-type DataSource<'t>() = 
-    abstract member getFromSource : 't -> DataResult<'t>
-    abstract member storeToSource : 't -> unit // just to show more options
-    default this.storeToSource _ = ()
 
-type Context<'t> = {cache: Cache<'t>; dataSource: DataSource<'t> }
+type Cache() = 
+    static member inline getOfCache cacheImpl data =
+        ( ^T : (member getFromCache : 't -> DataResult<'t> option) (cacheImpl, data))
+    static member inline storeOfCache cacheImpl data =
+        ( ^T : (member storeToCache : 't -> unit) (cacheImpl, data))
 
-let requestData (context:Context<UserName>) (userName:UserName) = monad {
-    match context.cache.getFromCache userName with
-    | Some dataResult -> return! map ((+) "cache: ") dataResult
-    | None -> return! map ((+) "source: ") (context.dataSource.getFromSource userName) }
+type DataSource() =
+    static member inline getOfSource dataSourceImpl data =
+        ( ^T : (member getFromSource : 't -> DataResult<'t>) (dataSourceImpl, data))
+    static member inline storeOfSource dataSourceImpl data =
+        ( ^T : (member storeToSource : 't -> unit) (dataSourceImpl, data))
+
+let inline requestData (cacheImpl: ^Cache) (dataSourceImpl: ^DataSource) (userName:UserName) = monad {
+    match Cache.getOfCache cacheImpl userName with
+    | Some dataResult -> 
+            return! map ((+) "cache: ") dataResult
+    | None -> 
+            return! map ((+) "source: ") (DataSource.getOfSource dataSourceImpl userName) }
+
 
 type Version = 
 | NotInCache
 | InCache
 
-let cache = function
-| NotInCache ->
-    { new Cache<'t> with
-        member this.getFromCache _ = None
-        member this.storeCache _ = () }
- | InCache -> 
-    { new Cache<'t> with
-        member this.getFromCache user = monad { 
-           return! DataResult user |> Some}
-        member this.storeCache _ = () }
+type CacheImpl (v:Version) = 
+        member this.getFromCache user = 
+            match v with
+            | InCache -> monad { 
+               return! DataResult user |> Some}
+            | NotInCache -> None
+        member this.storeCache _ = () 
 
-let dataSource = function
-| NotInCache ->
-    { new DataSource<'t>() with
-          member this.getFromSource user = monad { 
-               return! DataResult user } }
- | InCache -> 
-    { new DataSource<'t>() with
-          member this.getFromSource _  = 
-              raise (NotImplementedException())}
+       
+type DataSourceImpl (v:Version) = 
+          member this.getFromSource user =
+            match v with
+            | InCache -> 
+              raise (NotImplementedException())  
+            | NotInCache -> monad { 
+               return! DataResult user }    
+         
               
 [<EntryPoint>]
 let main argv =
-    let confWithCache cached = if cached then InCache else NotInCache
-    let context conf = { cache = cache conf; dataSource = dataSource conf }
-    let requestData' = flip requestData
-    confWithCache true |> context |> requestData' "john" |> printfn "%A"
-    confWithCache false |> context |> requestData' "john" |> printfn "%A"
-    0 // return an integer exit code
+    requestData (CacheImpl InCache) (DataSourceImpl InCache) "john" |> printfn "%A"
+    requestData (CacheImpl NotInCache) (DataSourceImpl NotInCache) "john" |> printfn "%A"
+    0 
